4c4d584063eeb81a192ebc9661aa62a35171b6f8
Breno Nogueira <breno.s.nogueira@hotmail.com>
2023-09-01 14:05:42 -0300

    Test Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,22 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
