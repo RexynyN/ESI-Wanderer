91b5886badb8030a20c8b64c91d2f6595a28a826
Luigi <tluigi@usp.br>
2023-09-01 13:48:09 -0300

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    def test
-        puts "Instance: #{self}"
-        puts "Instance Methods: #{self.methods}"
-      end
-end
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+  
+  end
